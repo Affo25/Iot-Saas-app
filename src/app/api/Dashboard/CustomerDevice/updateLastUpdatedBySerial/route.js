d9125e4da32888f6114b691bd51d41fe260cfb05
Afaq <afaq.affo17@gmail.com>
2025-07-01 16:34:40 +0500

    chnage

--- src/app/api/Dashboard/CustomerDevice/updateLastUpdatedBySerial/route.js
+++ src/app/api/Dashboard/CustomerDevice/updateLastUpdatedBySerial/route.js
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
-import dbConnect from '../../../../../lib/mongodb';
-import CustomerDevice from '../../../../../Models/CustomersDevice';
-import { authenticate } from '../../../../../lib/auth';
+import dbConnect from '../../../../lib/mongodb';
+import CustomerDevice from '../../../../Models/CustomersDevice';
+import { authenticate } from '../../../../lib/auth';
 
 export async function POST(request) {
   try {
